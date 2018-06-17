b1b7e3e43453fe9fe4f23736b220f510975fb567
Adrian Bona <adrian.bonaccini@gmail.com>
2018-06-17 16:49:16 -0300

    Greedy for Static

--- elizondo/main.cpp
+++ elizondo/main.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 #include <map>
 
-#include "static_player.hpp"
+#include "greedy_player.hpp"
 #include "player_controller.hpp"
 
 int main(int argc, char **argv ) {
-    player_controller<static_player> player(argv[1]);
+    player_controller<greedy_player> player(argv[1]);
     player.play();
     return 0;
 }
