b3ec33420d94e28979289e149d3f08254c8572fb
Adrian Bona <adrian.bonaccin@gmail.com>
2018-07-05 16:05:54 -0300

    Un poco de decencia no viene nada mal

--- elizondo/main_logical.cpp
+++ elizondo/main_logical.cpp
@@ -1,17 +1,18 @@
+#include <assert.h>
+
 #include "logical_board.cpp"
 #include "board_status.hpp"
 
 using namespace std;
 
-void Test_team_movements();
+void test_movements();
 
 int main() {
-    Test_team_movements();
-
+    test_movements();
     return 0;
 }
 
-void Test_team_movements() {
+void test_movements() {
     int n = 10;
     int m = 5;
     
@@ -38,7 +39,7 @@ void Test_team_movements() {
     vector<player_move> moves_B = {
         {0, "MOVIMIENTO", 0},
         {1, "MOVIMIENTO", 7},
-        {2, "MOVIMIENTO", 8}
+        {2, "MOVIMIENTO", 7}
     };
     
     for (int i = 0; i < 3; i++) {
@@ -51,36 +52,42 @@ void Test_team_movements() {
         board.teamB[i]->j = m-1;
     }
 
-    board.makeMove(moves_A, moves_B);
-    
-    board_status* updated = board.getCurrentState();
+    // Jugador 0 del equipo A está en la posición (0,0)
+    assert(board.teamA[0]->i == 0 && board.teamA[0]->j == 0);
 
-    if (board.teamA[0]->i != 1 && board.teamA[0]->j != 0) {
-        cout << "jugador 0 del equipo A no esta en su posicion" << endl;
-    }
-    if (board.teamA[1]->i != 1 && board.teamA[1]->j != 1) {
-        cout << "jugador 1 del equipo A no esta en su posicion" << endl;
-    }
-    if (board.teamA[2]->i != 3 && board.teamA[2]->j != 1) {
-        cout << "jugador 2 del equipo A no esta en su posicion" << endl;
-    }    
-    if (board.teamB[0]->i != 0 && board.teamB[0]->j != m-1) {
-        cout << "jugador 0 del equipo B no esta en su posicion" << endl;
-    }
-    if (board.teamB[1]->i != 0 && board.teamB[1]->j != m-2) {
-        cout << "jugador 1 del equipo B no esta en su posicion" << endl;
-    }
-    if (board.teamB[2]->i != 2 && board.teamB[2]->j != m-2) {
-        cout << "jugador 2 del equipo B no esta en su posicion" << endl;
-    }
+    // Jugador 1 del equipo A se movió a la posición (1,0)
+    assert(board.teamA[1]->i == 1 && board.teamA[1]->j == 0);
+
+    // Jugador 2 del equipo A se movió a la posición (2,0)
+    assert(board.teamA[2]->i == 2 && board.teamA[2]->j == 0);
+
+    // Jugador 0 del equipo B se movió a la posición (0,4)
+    assert(board.teamB[0]->i == 0 && board.teamB[0]->j == 4);
+
+    // Jugador 1 del equipo B se movió a la posición (1,4)
+    assert(board.teamB[1]->i == 1 && board.teamB[1]->j == 4);
+
+    // Jugador 2 del equipo B se movió a la posición (2,4)
+    assert(board.teamB[2]->i == 2 && board.teamB[2]->j == 4);
     
-    for (int i = 0; i < 3; ++i)
-    {
-        if (board.teamA[i]->i != updated->team[i].i || board.teamA[i]->j != updated->team[i].j) {
-            cout << "el status board no refleja la posicion del jugador " << i << " de mi equipo" << endl;  
-        }            
-        if (board.teamB[i]->i != updated->oponent_team[i].i || board.teamB[i]->j != updated->oponent_team[i].j) {
-            cout << "el status board no refleja la posicion del jugador " << i << " de mi equipo oponente" << endl;
-        }
-    }
+    board.makeMove(moves_A, moves_B);
+    board_status* updated = board.getCurrentState();
+    
+    // Jugador 0 del equipo A se movió a la posición (1,0)
+    assert(updated->team[0].i == 1 && updated->team[0].j == 0);
+
+    // Jugador 1 del equipo A se movió a la posición (1,1)
+    assert(updated->team[1].i == 1 && updated->team[1].j == 1);
+
+    // Jugador 2 del equipo A se movió a la posición (3,1)
+    assert(updated->team[2].i == 3 && updated->team[2].j == 1);
+
+    // Jugador 0 del equipo B se movió a la posición (0,4)
+    assert(updated->oponent_team[0].i == 0 && updated->oponent_team[0].j == m-1);
+
+    // Jugador 1 del equipo B se movió a la posición (2,3)
+    assert(updated->oponent_team[1].i == 2 && updated->oponent_team[1].j == m-2);
+
+    // Jugador 2 del equipo B se movió a la posición (3,3)
+    assert(updated->oponent_team[2].i == 3 && updated->oponent_team[2].j == m-2);
 }
